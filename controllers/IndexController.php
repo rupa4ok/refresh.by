42ea7850888f7437db5fa41b76cef7ae593dab15
rupak <rupak4@yandex.ru>
2018-09-28 20:30:35 +0300

    Убрал ajax из авторизации с главной

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -1,13 +1,10 @@
 <?php
 
-
 class IndexController
 {
     
-    
     public function action()
     {
-        
         require_once(ROOT . '/views/index.php');
         if (isset($_SESSION['name'])) {
             if ($_SESSION['name'] == 'admin')
@@ -15,10 +12,34 @@ class IndexController
                 header('Location: /admin1',true, 301); //редирект в админку админа
             }
             else {
-        
+            
             }
         }
         
         return true;
+    }
+    
+    public function Auth()
+    {
+    
+        $data1 = $_POST; //получаем данные из массива
+    
+        $errors1 = array();
+        $user = R::findOne('users','email = ?',array($data1['email']));//Проверка правильности email
+        if ( $user ) {
+            $hash = substr( $user->password, 0, 60 );
+            if (password_verify($data1['password'], $hash)) { //Проверка правильности пароля
+                //Логиним юзера
+                $_SESSION['logged_user'] = $user;
+                $_SESSION['name'] = $user->name;
+            
+                return;
+            } else {
+                echo "Пароль не верен";
+            }
+        }
+        else {
+            echo "Данный пользователь не найден";
+        }
     }
 }
--- views/index.php
+++ views/index.php
@@ -8,11 +8,11 @@
         <div class="container">
             <div class="row login-row">
                 <div class="col-md-4 col-md-offset-4 login-form">
-
-                    <!-- Tab panes -->
- 
+                                    <?php
+                                    $index = new IndexController();
+                                    $index->Auth(); ?>
                                     <div class="results1" style="color: red"></div>
-                                    <form method="POST" id="form1">
+                                    <form method="POST" id="form2">
                                         <input type="email" name="email" placeholder="Почта" value="<? echo @$data['email']; ?>">
                                         <input type="password" name="password"  placeholder="Пароль" value="<? echo @$data['password']; ?>">
                                         <button type="submit" name="do_login">Вход</button>
@@ -26,8 +26,7 @@
     <section>
         <div class="container">
             <div class="row">
-
-
+            
             </div>
         </div>
     </section>
